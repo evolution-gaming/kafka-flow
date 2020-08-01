d50822da9e2d95223e73695ac67a33333b2ef178
Scala Steward <me@scala-steward.org>
2020-08-02 00:47:37 +0200

    Update skafka to 10.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.0"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "1.8.1"
   val smetrics      = "com.evolutiongaming" %% "smetrics"    % "0.0.8"
-  val skafka        = "com.evolutiongaming" %% "skafka"      % "9.1.5"
+  val skafka        = "com.evolutiongaming" %% "skafka"      % "10.0.0"
 
   object Cats {
     private val version = "2.0.0"
