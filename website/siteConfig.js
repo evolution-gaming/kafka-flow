798085f9f94bf40da11609296005dde0e6b62dde
Ruslans Tarasovs <rtarasov@evolutiongaming.com>
2020-08-14 15:37:07 +0300

    Removed broken links.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'overview', label: 'Overview'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
