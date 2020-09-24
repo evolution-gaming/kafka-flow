346fb56217200345d8f2cff6d1a0e16115d09948
Ruslans Tarasovs <rtarasov@evolutiongaming.com>
2020-09-24 22:57:08 +0300

    Allow accessing state outside of `RecordFlow`.

--- core/src/main/scala/com/evolutiongaming/kafka/flow/RecordFlow.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/RecordFlow.scala
@@ -1,9 +1,13 @@
 package com.evolutiongaming.kafka.flow
 
 import cats.Applicative
+import cats.Monad
 import cats.data.NonEmptyList
 import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import cats.mtl.MonadState
 import cats.syntax.all._
+import com.olegpy.meow.effects._
 import persistence.Persistence
 import timer.ReadTimestamps
 
@@ -14,37 +18,47 @@ import timer.ReadTimestamps
   * if there is no specific key encountered in Kafka (i.e. for session expiration)
   * then `TimerFlow` could be used instead.
   */
-trait RecordFlow[F[_], E] {
+trait RecordFlow[F[_], A] {
 
-  def apply(records: NonEmptyList[E]): F[Unit]
+  def apply(records: NonEmptyList[A]): F[Unit]
 
 }
 object RecordFlow {
 
   /** Create flow which persists snapshots, events and restores state if needed */
-  def of[F[_]: Sync: KeyContext, S, E](
-    fold: FoldOption[F, S, E],
-    persistence: Persistence[F, S, E]
-  ): F[RecordFlow[F, E]] =
+  def of[F[_]: Sync: KeyContext, S, A](
+    fold: FoldOption[F, S, A],
+    persistence: Persistence[F, S, A]
+  ): F[RecordFlow[F, A]] = Ref.of(none[S]) flatMap { storage =>
+    of(storage.stateInstance, fold, persistence)
+  }
+
+  /** Create flow which persists snapshots, events and restores state if needed */
+  def of[F[_]: Monad: KeyContext, S, A](
+    storage: MonadState[F, Option[S]],
+    fold: FoldOption[F, S, A],
+    persistence: Persistence[F, S, A]
+  ): F[RecordFlow[F, A]] =
     for {
       state <- persistence.read
       logIfRecovered = state as KeyContext[F].log.info(s"recovered state")
       _ <- logIfRecovered.sequence_
-      foldToState <- FoldToState.of(state, fold, persistence)
+      _ <- storage.set(state)
+      foldToState = FoldToState(storage, fold, persistence)
     } yield records => foldToState(records)
 
 
   /** Does not save anything to the database */
-  def transient[F[_]: Sync: KeyContext: ReadTimestamps, K, S, E](
-    fold: FoldOption[F, S, E]
-  ): F[RecordFlow[F, E]] =
+  def transient[F[_]: Sync: KeyContext: ReadTimestamps, K, S, A](
+    fold: FoldOption[F, S, A]
+  ): F[RecordFlow[F, A]] =
     for {
       startedAt <- ReadTimestamps[F].current
       _ <- KeyContext[F].hold(startedAt.offset)
-      foldToState <- FoldToState.of(None, fold, Persistence.empty[F, S, E])
+      foldToState <- FoldToState.of(None, fold, Persistence.empty[F, S, A])
     } yield records => foldToState(records)
 
-  def empty[F[_]: Applicative, E]: RecordFlow[F, E] = { _ =>
+  def empty[F[_]: Applicative, A]: RecordFlow[F, A] = { _ =>
     ().pure[F]
   }
 
