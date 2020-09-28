7ec31a64f8c8675570c07d4b30e5359e425ea67b
Ruslans Tarasovs <ruslan@tarasovs.com>
2020-09-28 15:34:10 +0300

    Make it backwards compatible.

--- core/src/main/scala/com/evolutiongaming/kafka/flow/Tick.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/Tick.scala
@@ -8,6 +8,11 @@ case class Tick[F[_], S](run: S => F[S]) {
   /** Alias for `run` */
   def apply(s: S): F[S] = run(s)
 
+  /** Constructs `Fold` which ignores all incoming `A` elements and just triggers underlying `run` */
+  def toFold[A]: Fold[F, S, A] = Fold { (s, _) =>
+    run(s)
+  }
+
 }
 object Tick {
 
--- core/src/main/scala/com/evolutiongaming/kafka/flow/TickOption.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/TickOption.scala
@@ -7,8 +7,14 @@ case class TickOption[F[_], S](value: Tick[F, Option[S]]) {
   /** Alias for `value.run` */
   def apply(s: Option[S]): F[Option[S]] = value(s)
 
+  /** Constructs `FoldOption` which ignores all incoming `A` elements and just triggers underlying `value.run` */
+  def toFold[A]: FoldOption[F, S, A] = FoldOption(value.toFold[A])
+
 }
 object TickOption {
+
+  def of[F[_], S](run: Option[S] => F[Option[S]]): TickOption[F, S] =
+    TickOption(Tick(run))
 
   def unit[F[_]: Applicative, S]: TickOption[F, S] = TickOption(Tick.unit)
 
