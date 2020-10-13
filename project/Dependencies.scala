8e204f85fb2fb7284ebb5e2a9d11549a134f3323
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-13 22:32:07 +0200

    Update munit to 0.7.14 (#88)
    Co-authored-by: Ruslans Tarasovs <49794769+rtar@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val munit  = "org.scalameta"       %% "munit"            % "0.7.13"
+  val munit  = "org.scalameta"       %% "munit"            % "0.7.14"
   val weaver = "com.disneystreaming" %% "weaver-framework" % "0.5.0"
 
   val cassandraLauncher = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.4"
