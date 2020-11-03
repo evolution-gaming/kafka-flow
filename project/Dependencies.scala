7bffaf87ed8d570ade9e0de9731103acd2f629fa
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-03 14:26:38 +0100

    Update scache to 3.2.0 (#96)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val cassandraLauncher = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.4"
   val catsHelper        = "com.evolutiongaming" %% "cats-helper"        % "2.1.2"
-  val scache            = "com.evolutiongaming" %% "scache"             % "3.1.0"
+  val scache            = "com.evolutiongaming" %% "scache"             % "3.2.0"
   val skafka            = "com.evolutiongaming" %% "skafka"             % "11.0.0"
   val smetrics          = "com.evolutiongaming" %% "smetrics"           % "0.1.2"
   val sstream           = "com.evolutiongaming" %% "sstream"            % "0.2.1"
