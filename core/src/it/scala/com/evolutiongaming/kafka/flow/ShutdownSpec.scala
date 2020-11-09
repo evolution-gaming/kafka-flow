9a7731bbc7fce7b595ec95b8c21ed93e2bcf1534
Ruslans Tarasovs <ruslan@tarasovs.com>
2020-11-09 14:33:18 +0200

    Reproduced issue from testing environments.

--- core/src/it/scala/com/evolutiongaming/kafka/flow/ShutdownSpec.scala
+++ core/src/it/scala/com/evolutiongaming/kafka/flow/ShutdownSpec.scala
@@ -3,6 +3,7 @@ package com.evolutiongaming.kafka.flow
 import ShutdownSpec._
 import cats.Applicative
 import cats.Defer
+import cats.data.NonEmptyMap
 import cats.data.NonEmptySet
 import cats.effect.IO
 import cats.effect.Resource
@@ -10,12 +11,16 @@ import cats.effect.concurrent.Deferred
 import cats.effect.concurrent.Ref
 import cats.syntax.all._
 import com.evolutiongaming.catshelper.LogOf
+import com.evolutiongaming.kafka.flow.kafka.Consumer
 import com.evolutiongaming.kafka.flow.kafka.KafkaModule
 import com.evolutiongaming.kafka.journal.ConsRecords
 import com.evolutiongaming.retry.Retry
 import com.evolutiongaming.skafka.CommonConfig
 import com.evolutiongaming.skafka.Offset
+import com.evolutiongaming.skafka.OffsetAndMetadata
 import com.evolutiongaming.skafka.Partition
+import com.evolutiongaming.skafka.Topic
+import com.evolutiongaming.skafka.TopicPartition
 import com.evolutiongaming.skafka.producer.ProducerConfig
 import com.evolutiongaming.skafka.producer.ProducerRecord
 import com.evolutiongaming.sstream.Stream
@@ -40,7 +45,7 @@ class ShutdownSpec(val globalResources: GlobalResources) extends KafkaSpec {
     }
 
     def program(
-      flow: TopicFlow[IO],
+      flowOf: TopicFlowOf[IO],
       finished: Deferred[IO, Unit]
     ) = Stream.lift(LogOf.slf4j[IO]) flatMap { implicit logOf =>
       for {
@@ -51,7 +56,7 @@ class ShutdownSpec(val globalResources: GlobalResources) extends KafkaSpec {
           consumer = kafka.consumerOf("UnsubscribeSpec-groupId"),
           flowOf = ConsumerFlowOf[IO](
             topic = "UnsubscribeSpec-topic",
-            flowOf = { (_, _) => Resource.pure(flow) }
+            flowOf = flowOf
           )
         )
         // inform that it was processed, but do not finish the stream
@@ -61,11 +66,11 @@ class ShutdownSpec(val globalResources: GlobalResources) extends KafkaSpec {
     }
 
     for {
-      state         <- Ref.of(Set.empty[Partition])
-      finished      <- Deferred[IO, Unit]
+      state      <- Ref.of(Set.empty[Partition])
+      finished   <- Deferred[IO, Unit]
       // start a stream
-      flow       = topicFlow(state)
-      program    <- program(flow, finished).toList.start
+      flowOf      = topicFlowOf(state)
+      program    <- program(flowOf, finished).toList.start
       // wait for first record to process
       _          <- finished.get
       // validate subscriptions in active flow
@@ -83,21 +88,25 @@ class ShutdownSpec(val globalResources: GlobalResources) extends KafkaSpec {
 }
 object ShutdownSpec {
 
-  def topicFlow(state: Ref[IO, Set[Partition]]): TopicFlow[IO] =
-    new TopicFlow[IO] {
-      implicit val timer = IO.timer(ExecutionContext.global)
-      def apply(records: ConsRecords) = IO.unit
-      def add(partitionsAndOffsets: NonEmptySet[(Partition, Offset)]) = {
-        val partitions = partitionsAndOffsets map (_._1)
-        state update (_ ++ partitions.toList)
-      }
-      def remove(partitions: NonEmptySet[Partition]) =
-        // we wait for a second here to ensure the call is blocking
-        // i.e. if we update state immediately, the test might pass
-        // event if the call is non-blocking
-        IO.sleep(1.second) *> {
-          state update (_ -- partitions.toList)
+  def topicFlowOf(state: Ref[IO, Set[Partition]]): TopicFlowOf[IO] =
+    (consumer, topic) => Resource.pure[IO, TopicFlow[IO]] {
+      new TopicFlow[IO] {
+        //implicit val timer = IO.timer(ExecutionContext.global)
+        def apply(records: ConsRecords) = IO.unit
+        def add(partitionsAndOffsets: NonEmptySet[(Partition, Offset)]) = {
+          val partitions = partitionsAndOffsets map (_._1)
+          state update (_ ++ partitions.toList)
         }
+        def remove(partitions: NonEmptySet[Partition]) =
+          // we wait for a second here to ensure the call is blocking
+          // i.e. if we update state immediately, the test might pass
+          // event if the call is non-blocking
+          consumer.commit(NonEmptyMap.of(
+            TopicPartition(topic, Partition.min) -> OffsetAndMetadata(Offset.unsafe(2))
+          )) *> {
+            state update (_ -- partitions.toList)
+          }
+      }
     }
 
 }
