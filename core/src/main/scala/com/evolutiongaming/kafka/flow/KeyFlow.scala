d917516fca943b2eb4e7580772cc5acd5e6f23b1
Ruslans Tarasovs <ruslan@tarasovs.com>
2020-12-01 21:23:02 +0200

    Added a comment, tests are pending.

--- core/src/main/scala/com/evolutiongaming/kafka/flow/KeyFlow.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/KeyFlow.scala
@@ -47,6 +47,9 @@ object KeyFlow {
     for {
       state <- persistence.read(KeyContext[F].log)
       _ <- storage.set(state)
+      // we should not run any timers if there was decision
+      // by fold or tick to run the state, because in this
+      // case we may flush the key which was already removed
       timerCancelled = storage inspect (_.isEmpty)
       foldToState = FoldToState(storage, fold, persistence)
       tickToState = TickToState(storage, tick, persistence)
@@ -66,6 +69,9 @@ object KeyFlow {
       startedAt <- ReadTimestamps[F].current
       _ <- KeyContext[F].hold(startedAt.offset)
       storage <- Ref.of(none[S])
+      // we should not run any timers if there was decision
+      // by fold or tick to run the state, because in this
+      // case we may flush the key which was already removed
       timerCancelled = storage.get map (_.isEmpty)
       foldToState = FoldToState(storage.stateInstance, fold, Persistence.empty[F, S, A])
       tickToState = TickToState(storage.stateInstance, tick, Persistence.empty[F, S, A])
