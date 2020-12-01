ea33af447567eb4b08823aeb0b5db915aaee2858
Ruslans Tarasovs <ruslan@tarasovs.com>
2020-12-01 22:51:50 +0200

    Added type aliases with fixed event to simplify type inference.

---
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/package.scala
@@ -0,0 +1,8 @@
+package com.evolutiongaming.kafka
+
+import com.evolutiongaming.kafka.journal.ConsRecord
+
+package object flow {
+  type FoldCons[F[_], S] = Fold[F, S, ConsRecord]
+  type FoldOptionCons[F[_], S] = FoldOption[F, S, ConsRecord]
+}
---
+++ metrics/src/test/scala/com/evolutiongaming/kafka/flow/FoldMetricsSpec.scala
@@ -0,0 +1,20 @@
+package com.evolutiongaming.kafka.flow
+
+import FoldMetrics._
+import com.evolutiongaming.kafka.journal.ConsRecord
+import com.evolutiongaming.smetrics.CollectorRegistry
+import com.evolutiongaming.smetrics.MeasureDuration
+import metrics.syntax._
+import munit.FunSuite
+
+class FoldMetricsSpec extends FunSuite {
+
+  type F[T] = Option[T]
+
+  test("having MetricsKOf enables withCollectorRegistry syntax") {
+    implicit val measureDuration = MeasureDuration.empty[F]
+    val fold: FoldOptionCons[F, Int] = FoldOption.empty[F, Int, ConsRecord]
+    fold.withCollectorRegistry(CollectorRegistry.empty[F])
+  }
+
+}
