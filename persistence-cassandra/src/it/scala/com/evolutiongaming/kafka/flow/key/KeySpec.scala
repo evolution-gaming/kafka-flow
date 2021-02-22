a3a6ae25bf0b960aa0ebefe2fb9fc4874e6ad244
Nikita Matveenko <nmatveenko@evolutiongaming.com>
2021-02-22 22:19:05 +0100

    remove unused method in KeyDatabase: `def all(applicationId: String, groupId: String): Stream[F, K]`

--- persistence-cassandra/src/it/scala/com/evolutiongaming/kafka/flow/key/KeySpec.scala
+++ persistence-cassandra/src/it/scala/com/evolutiongaming/kafka/flow/key/KeySpec.scala
@@ -20,25 +20,19 @@ class KeySpec(val globalResources: GlobalResources) extends CassandraSpec {
     val key3 = KafkaKey("KeySpec", "integration-tests-1", partition2, "queries.key3")
     for {
       keys <- CassandraKeys.withSchema(cassandra.session, cassandra.sync)
-      allKeysBeforeTest <- keys.all("KeySpec", "integration-tests-1").toList
       partition1KeysBeforeTest <- keys.all("KeySpec", "integration-tests-1", partition1).toList
       partition2KeysBeforeTest <- keys.all("KeySpec", "integration-tests-1", partition2).toList
       _ <- List(key1, key2, key3) traverse_ keys.persist
-      allKeysAfterPersist <- keys.all("KeySpec", "integration-tests-1").toList
       partition1KeysAfterPersist <- keys.all("KeySpec", "integration-tests-1", partition1).toList
       partition2KeysAfterPersist <- keys.all("KeySpec", "integration-tests-1", partition2).toList
       _ <- List(key1, key2, key3) traverse_ keys.delete
-      allKeysAfterDelete <- keys.all("KeySpec", "integration-tests-1").toList
       partition1KeysAfterDelete <- keys.all("KeySpec", "integration-tests-1", partition1).toList
       partition2KeysAfterDelete <- keys.all("KeySpec", "integration-tests-1", partition2).toList
     } yield {
-      expect(allKeysBeforeTest.isEmpty) and
       expect(partition1KeysBeforeTest.isEmpty) and
       expect(partition2KeysBeforeTest.isEmpty) and
-      expect(allKeysAfterPersist.length == 3) and
       expect(partition1KeysAfterPersist.length == 1) and
       expect(partition2KeysAfterPersist.length == 2) and
-      expect(allKeysAfterDelete.isEmpty) and
       expect(partition1KeysAfterDelete.isEmpty) and
       expect(partition2KeysAfterDelete.isEmpty)
     }
@@ -50,7 +44,7 @@ class KeySpec(val globalResources: GlobalResources) extends CassandraSpec {
       session    = CassandraSessionStub.injectFailures(cassandra.session, failAfter)
       keys      <- CassandraKeys.withSchema(session, cassandra.sync)
       _ <- failAfter.set(1)
-      keys <- keys.all("KeySpec", "integration-tests-1").toList.attempt
+      keys <- keys.all("KeySpec", "integration-tests-1", TopicPartition("topic", Partition.min)).toList.attempt
     } yield expect(keys.isLeft)
   }
 
