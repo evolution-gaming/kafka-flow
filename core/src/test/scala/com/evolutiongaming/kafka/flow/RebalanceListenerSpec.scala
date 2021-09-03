a9ba151a871bfca2d09fd493857a896437d9140a
Z1kkurat <kshelopugin@gmail.com>
2021-09-03 12:19:20 +0200

    Bump kafka-journal and other dependencies, migrate to new RebalanceCallback1

--- core/src/test/scala/com/evolutiongaming/kafka/flow/RebalanceListenerSpec.scala
+++ core/src/test/scala/com/evolutiongaming/kafka/flow/RebalanceListenerSpec.scala
@@ -1,22 +1,24 @@
 package com.evolutiongaming.kafka.flow
 
-import cats.data.{NonEmptyMap, NonEmptySet, State}
+import cats.data.{NonEmptyMap, NonEmptySet, StateT}
 import cats.syntax.all._
 import com.evolutiongaming.catshelper.LogOf
 import com.evolutiongaming.kafka.flow.RebalanceListenerSpec._
 import com.evolutiongaming.kafka.flow.kafka.Consumer
 import com.evolutiongaming.kafka.journal.ConsRecords
-import com.evolutiongaming.skafka.consumer.{RebalanceListener => SRebalanceListener}
 import com.evolutiongaming.skafka._
+import com.evolutiongaming.skafka.consumer.{RebalanceListener1 => SRebalanceListener}
 import munit.FunSuite
 
 import scala.concurrent.duration.FiniteDuration
+import scala.util.Try
 
 class RebalanceListenerSpec extends FunSuite {
 
+  private val noopConsumer = new Consumer.NoopRebalanceConsumer
 
   test("Listener should handle partitions assignment") {
-    val result = Fixture
+    val result: Try[Context] = Fixture
       .listener(topic1, topic2)
       .onPartitionsAssigned(
         NonEmptySet.of(
@@ -26,18 +28,18 @@ class RebalanceListenerSpec extends FunSuite {
           TopicPartition(topic2, partition4)
         )
       )
+      .toF(noopConsumer)
       .runS(Context())
-      .value
 
     val expected = Map(
       topic1 -> Action.Add(NonEmptySet.of(partition1 -> offset, partition2 -> offset)),
       topic2 -> Action.Add(NonEmptySet.of(partition3 -> offset, partition4 -> offset))
     )
-    assertEquals(result.actions, expected)
+    assertEquals(result.map(_.actions), expected.pure[Try])
   }
 
   test("Listener should handle partitions revoke") {
-    val result = Fixture
+    val result: Try[Context] = Fixture
       .listener(topic1, topic2)
       .onPartitionsRevoked(
         NonEmptySet.of(
@@ -45,18 +47,18 @@ class RebalanceListenerSpec extends FunSuite {
           TopicPartition(topic2, partition3)
         )
       )
+      .toF(noopConsumer)
       .runS(Context())
-      .value
 
     val expected = Map(
       topic1 -> Action.Remove(NonEmptySet.of(partition1)),
       topic2 -> Action.Remove(NonEmptySet.of(partition3))
     )
-    assertEquals(result.actions, expected)
+    assertEquals(result.map(_.actions), expected.pure[Try])
   }
 
   test("Listener should handle partitions lost") {
-    val result = Fixture
+    val result: Try[Context] = Fixture
       .listener(topic1, topic2)
       .onPartitionsLost(
         NonEmptySet.of(
@@ -64,14 +66,14 @@ class RebalanceListenerSpec extends FunSuite {
           TopicPartition(topic2, partition3)
         )
       )
+      .toF(noopConsumer)
       .runS(Context())
-      .value
 
     val expected = Map(
       topic1 -> Action.Remove(NonEmptySet.of(partition1)),
       topic2 -> Action.Remove(NonEmptySet.of(partition3))
     )
-    assertEquals(result.actions, expected)
+    assertEquals(result.map(_.actions), expected.pure[Try])
   }
 
 }
@@ -86,7 +88,7 @@ object RebalanceListenerSpec {
   val partition4 = Partition.unsafe(4)
   val offset = Offset.unsafe(0)
 
-  type F[A] = State[Context, A]
+  type F[A] = StateT[Try, Context, A]
 
   implicit val logOf = LogOf.empty[F]
 
@@ -114,14 +116,13 @@ object RebalanceListenerSpec {
     def flow(topic: String) = new TopicFlow[F] {
       def apply(records: ConsRecords): F[Unit] = ().pure[F]
       def add(partitions: NonEmptySet[(Partition, Offset)]): F[Unit] =
-        State modify (_.add(topic, Action.Add(partitions)))
+        StateT.modify(_.add(topic, Action.Add(partitions)))
       def remove(partitions: NonEmptySet[Partition]): F[Unit] =
-        State modify (_.add(topic, Action.Remove(partitions)))
+        StateT.modify(_.add(topic, Action.Remove(partitions)))
     }
 
-    def listener(topics: Topic*) =
+    def listener(topics: Topic*): SRebalanceListener[F] =
       RebalanceListener(
-        consumer = consumer,
         flows = topics.map(topic => topic -> flow(topic)).toMap
       )
   }
