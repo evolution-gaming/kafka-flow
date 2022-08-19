1523f4964e6661a9ec6560d1a6673fa992ce65e5
Z1kkurat <kshelopugin@gmail.com>
2022-08-19 12:01:27 +0200

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,23 +2,23 @@ import sbt._
 
 object Dependencies {
 
-  val munit = "org.scalameta" %% "munit" % "0.7.22"
-  val scribe = "com.outr" %% "scribe-slf4j" % "3.5.0"
-  val weaver = "com.disneystreaming" %% "weaver-cats" % "0.7.11"
+  val munit = "org.scalameta" %% "munit" % "0.7.29"
+  val scribe = "com.outr" %% "scribe-slf4j" % "3.10.1"
+  val weaver = "com.disneystreaming" %% "weaver-cats" % "0.7.14"
 
   val cassandraLauncher = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.4"
   val catsHelper = "com.evolutiongaming" %% "cats-helper" % "3.1.0"
   val kafkaLauncher = "com.evolutiongaming" %% "kafka-launcher" % "0.0.11"
   val scache = "com.evolutiongaming" %% "scache" % "4.0.0"
-  val skafka = "com.evolutiongaming" %% "skafka" % "12.1.3"
+  val skafka = "com.evolutiongaming" %% "skafka" % "12.2.2"
   val smetrics = "com.evolutiongaming" %% "smetrics" % "1.0.4"
   val sstream = "com.evolutiongaming" %% "sstream" % "1.0.1"
 
   object Cats {
-    private val version = "2.7.0"
-    private val effectVersion = "3.3.7"
+    private val version = "2.8.0"
+    private val effectVersion = "3.3.14"
     val core = "org.typelevel" %% "cats-core" % version
-    val mtl = "org.typelevel" %% "cats-mtl" % "1.2.1"
+    val mtl = "org.typelevel" %% "cats-mtl" % "1.3.0"
     val effect = "org.typelevel" %% "cats-effect" % effectVersion
     val effectTestkit = "org.typelevel" %% "cats-effect-testkit" % effectVersion
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,13 +5,13 @@ libraryDependencySchemes ++= Seq(
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.2")
 
-addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.1")
+addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.2")
 
-addSbtPlugin("com.github.sbt" % "sbt-release" % "1.0.15")
+addSbtPlugin("com.github.sbt" % "sbt-release" % "1.1.0")
 
-addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.2.18" )
+addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.3.3" )
 
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.2")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
 
 addSbtPlugin("com.evolution" % "sbt-scalac-opts-plugin" % "0.0.9")
 
