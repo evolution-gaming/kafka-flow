6d144aa2508793da9800525bfe909620579731b1
Z1kkurat <kshelopugin@gmail.com>
2022-08-22 11:32:02 +0200

    Update kafka-launcher to 0.0.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
   val cassandraLauncher = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.4"
   val catsHelper = "com.evolutiongaming" %% "cats-helper" % "3.1.0"
-  val kafkaLauncher = "com.evolutiongaming" %% "kafka-launcher" % "0.0.11"
+  val kafkaLauncher = "com.evolutiongaming" %% "kafka-launcher" % "0.0.12"
   val scache = "com.evolutiongaming" %% "scache" % "4.0.0"
   val skafka = "com.evolutiongaming" %% "skafka" % "14.0.0"
   val smetrics = "com.evolutiongaming" %% "smetrics" % "1.0.4"
