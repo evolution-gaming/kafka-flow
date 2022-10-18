d109f9aef0293f76ab92221ae06e1ab2ba4b2e17
Kirill Shelopugin <kshelopugin@gmail.com>
2022-10-18 13:00:06 +0200

    Toggleable compression for application state (#387)

---
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/persistence/compression/HeaderAndPayload.scala
@@ -0,0 +1,26 @@
+package com.evolutiongaming.kafka.flow.persistence.compression
+
+import cats.MonadThrow
+import cats.syntax.all._
+import scodec.Attempt
+import scodec.bits.ByteVector
+import scodec.codecs._
+
+private[compression] object HeaderAndPayload {
+
+  private val codec = variableSizeBytes(int32, bytes) ~ bytes
+
+  def toBytes[F[_]: MonadThrow](header: ByteVector, payload: ByteVector): F[ByteVector] = {
+    codec.encode(header ~ payload) match {
+      case Attempt.Successful(value) => value.bytes.pure[F]
+      case Attempt.Failure(e)        => CompressionError(e.message).raiseError
+    }
+  }
+
+  def fromBytes[F[_]: MonadThrow](bytes: ByteVector): F[(ByteVector, ByteVector)] = {
+    codec.decode(bytes.bits) match {
+      case Attempt.Successful(value) => value.value.pure[F]
+      case Attempt.Failure(e)        => CompressionError(e.message).raiseError
+    }
+  }
+}
