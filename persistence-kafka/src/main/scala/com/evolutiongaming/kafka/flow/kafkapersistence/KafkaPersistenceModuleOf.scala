a7da2739a7ffd500f8d775cd878f35dfda77f320
Kirill Shelopugin <kshelopugin@gmail.com>
2022-11-23 10:58:48 +0100

    Explicitly pass Producer to KafkaPersistenceModuleOf [CE2] (#406)
    * Explicitly pass Producer to KafkaPersistenceModuleOf
    
    * Fix scaladoc

--- persistence-kafka/src/main/scala/com/evolutiongaming/kafka/flow/kafkapersistence/KafkaPersistenceModuleOf.scala
+++ persistence-kafka/src/main/scala/com/evolutiongaming/kafka/flow/kafkapersistence/KafkaPersistenceModuleOf.scala
@@ -4,9 +4,9 @@ import cats.Parallel
 import cats.effect.{Concurrent, Resource}
 import com.evolutiongaming.catshelper.LogOf
 import com.evolutiongaming.kafka.flow.FlowMetrics
-import com.evolutiongaming.skafka.consumer.{ConsumerConfig, ConsumerOf}
-import com.evolutiongaming.skafka.producer.{ProducerConfig, ProducerOf}
 import com.evolutiongaming.skafka._
+import com.evolutiongaming.skafka.consumer.{ConsumerConfig, ConsumerOf}
+import com.evolutiongaming.skafka.producer.{Producer, ProducerConfig, ProducerOf}
 
 /** Convenience factory trait to create an instance of [[KafkaPersistenceModule]] for an assigned partition */
 trait KafkaPersistenceModuleOf[F[_], S] {
@@ -15,33 +15,70 @@ trait KafkaPersistenceModuleOf[F[_], S] {
 
 object KafkaPersistenceModuleOf {
 
+  def caching[F[_]: LogOf: Concurrent: Parallel, S](
+    consumerOf: ConsumerOf[F],
+    producer: Producer[F],
+    consumerConfig: ConsumerConfig,
+    snapshotTopic: Topic
+  )(implicit
+    fromBytesKey: FromBytes[F, String],
+    fromBytesState: FromBytes[F, S],
+    toBytesState: ToBytes[F, S]
+  ): KafkaPersistenceModuleOf[F, S] = caching(consumerOf, producer, consumerConfig, snapshotTopic, FlowMetrics.empty[F])
+
   /** Create a [[KafkaPersistenceModuleOf]] factory instance which will then produce a caching implementation of [[KafkaPersistenceModule]]
-    * for an assigned partition. See [[KafkaPersistenceModule.caching]] documentation for further details.
+    * for an assigned partition. See `KafkaPersistenceModule.caching` documentation for further details.
     * Needed mostly as a convenient helper to provide a part of necessary parameters beforehand and pass this factory around.
     * @param consumerOf factory of consumers
-    * @param producerOf factory of producers
+    * @param producer snapshot producer
     * @param consumerConfig consumer config to be used when creating snapshot topic consumer
-    * @param producerConfig producer config to be used when creating snapshot topic producer
     * @param snapshotTopic snapshot topic name (should be configured as a 'compacted' topic)
     * @param metrics instance of `FlowMetrics` for [[KafkaPersistenceModule]]
     */
-  def caching[F[_]: LogOf: Concurrent: Parallel: FromBytes[*[_], String]: ToBytes[*[_], S], S: FromBytes[F, *]](
+  def caching[F[_]: LogOf: Concurrent: Parallel, S](
+    consumerOf: ConsumerOf[F],
+    producer: Producer[F],
+    consumerConfig: ConsumerConfig,
+    snapshotTopic: Topic,
+    metrics: FlowMetrics[F]
+  )(implicit
+    fromBytesKey: FromBytes[F, String],
+    fromBytesState: FromBytes[F, S],
+    toBytesState: ToBytes[F, S]
+  ): KafkaPersistenceModuleOf[F, S] = new KafkaPersistenceModuleOf[F, S] {
+    override def make(partition: Partition): Resource[F, KafkaPersistenceModule[F, S]] = {
+      KafkaPersistenceModule.caching[F, S](
+        consumerOf = consumerOf,
+        producer = producer,
+        consumerConfig = consumerConfig,
+        snapshotTopicPartition = TopicPartition(snapshotTopic, partition),
+        metrics = metrics
+      )
+    }
+  }
+
+  @deprecated("Use `caching` with passing a Producer to avoid per-partition Producer creation", since = "0.8.5")
+  def caching[F[_]: LogOf: Concurrent: Parallel, S](
     consumerOf: ConsumerOf[F],
     producerOf: ProducerOf[F],
     consumerConfig: ConsumerConfig,
     producerConfig: ProducerConfig,
     snapshotTopic: Topic,
     metrics: FlowMetrics[F] = FlowMetrics.empty[F]
-  ): KafkaPersistenceModuleOf[F, S] = new KafkaPersistenceModuleOf[F, S] {
-    override def make(partition: Partition): Resource[F, KafkaPersistenceModule[F, S]] = {
-      KafkaPersistenceModule.caching[F, S](
-        consumerOf = consumerOf,
-        producerOf = producerOf,
-        consumerConfig = consumerConfig,
-        producerConfig = producerConfig,
-        snapshotTopicPartition = TopicPartition(snapshotTopic, partition),
-        metrics = metrics
-      )
+  )(implicit
+    fromBytesKey: FromBytes[F, String],
+    fromBytesState: FromBytes[F, S],
+    toBytesState: ToBytes[F, S]
+  ): KafkaPersistenceModuleOf[F, S] =
+    new KafkaPersistenceModuleOf[F, S] {
+      override def make(partition: Partition): Resource[F, KafkaPersistenceModule[F, S]] =
+        KafkaPersistenceModule.caching(
+          consumerOf = consumerOf,
+          producerOf = producerOf,
+          consumerConfig = consumerConfig,
+          producerConfig = producerConfig,
+          snapshotTopicPartition = TopicPartition(snapshotTopic, partition),
+          metrics = metrics
+        )
     }
-  }
 }
