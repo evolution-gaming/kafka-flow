ddeaafee9cbcaa906f660f0fbe48cec50d167401
Scala Steward <scala_steward@virtuslab.com>
2023-03-16 16:17:56 +0000

    Update cats-core to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,33 +2,33 @@ import sbt._
 
 object Dependencies {
 
-  val catsHelper = "com.evolutiongaming" %% "cats-helper" % "3.4.0"
+  val catsHelper        = "com.evolutiongaming" %% "cats-helper"         % "3.4.0"
   val catsHelperLogback = "com.evolutiongaming" %% "cats-helper-logback" % "3.4.0"
-  val smetrics = "com.evolutiongaming" %% "smetrics" % "1.0.7"
-  val scache = "com.evolutiongaming" %% "scache" % "4.3.1"
-  val skafka = "com.evolutiongaming" %% "skafka" % "15.0.1"
-  val sstream = "com.evolutiongaming" %% "sstream" % "1.0.1"
+  val smetrics          = "com.evolutiongaming" %% "smetrics"            % "1.0.7"
+  val scache            = "com.evolutiongaming" %% "scache"              % "4.3.1"
+  val skafka            = "com.evolutiongaming" %% "skafka"              % "15.0.1"
+  val sstream           = "com.evolutiongaming" %% "sstream"             % "1.0.1"
 
   object Cats {
-    private val version = "2.7.0"
+    private val version       = "2.9.0"
     private val effectVersion = "3.4.8"
-    val core = "org.typelevel" %% "cats-core" % version
-    val mtl = "org.typelevel" %% "cats-mtl" % "1.2.1"
-    val effect = "org.typelevel" %% "cats-effect" % effectVersion
-    val effectTestkit = "org.typelevel" %% "cats-effect-testkit" % effectVersion
+    val core                  = "org.typelevel" %% "cats-core"           % version
+    val mtl                   = "org.typelevel" %% "cats-mtl"            % "1.2.1"
+    val effect                = "org.typelevel" %% "cats-effect"         % effectVersion
+    val effectTestkit         = "org.typelevel" %% "cats-effect-testkit" % effectVersion
   }
 
   object KafkaJournal {
     private val version = "1.1.0"
-    val journal = "com.evolutiongaming" %% "kafka-journal" % version
-    val cassandra = "com.evolutiongaming" %% "kafka-journal-eventual-cassandra" % version
-    val persistence = "com.evolutiongaming" %% "kafka-journal-persistence" % version
+    val journal         = "com.evolutiongaming" %% "kafka-journal"                    % version
+    val cassandra       = "com.evolutiongaming" %% "kafka-journal-eventual-cassandra" % version
+    val persistence     = "com.evolutiongaming" %% "kafka-journal-persistence"        % version
   }
 
   object Monocle {
     private val version = "2.1.0"
-    val core = "com.github.julien-truffaut" %% "monocle-core" % version
-    val `macro` = "com.github.julien-truffaut" %% "monocle-macro" % version
+    val core            = "com.github.julien-truffaut" %% "monocle-core"  % version
+    val `macro`         = "com.github.julien-truffaut" %% "monocle-macro" % version
   }
 
   object Testing {
@@ -36,9 +36,9 @@ object Dependencies {
 
     object Testcontainers {
       private val version = "0.40.12"
-      val munit = "com.dimafeng" %% "testcontainers-scala-munit" % version
-      val kafka = "com.dimafeng" %% "testcontainers-scala-kafka" % version
-      val cassandra = "com.dimafeng" %% "testcontainers-scala-cassandra" % version
+      val munit           = "com.dimafeng" %% "testcontainers-scala-munit"     % version
+      val kafka           = "com.dimafeng" %% "testcontainers-scala-kafka"     % version
+      val cassandra       = "com.dimafeng" %% "testcontainers-scala-cassandra" % version
     }
   }
 
