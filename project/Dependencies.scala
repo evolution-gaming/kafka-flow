5f3be16a579146b5e86034b1d3f0fd2480935a42
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-05-16 14:26:36 +0200

    bump pureconfig to 0.17.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   }
 
   object KafkaJournal {
-    private val version = "1.1.1"
+    private val version = "2.1.0"
     val journal         = "com.evolutiongaming" %% "kafka-journal"                    % version
     val cassandra       = "com.evolutiongaming" %% "kafka-journal-eventual-cassandra" % version
     val persistence     = "com.evolutiongaming" %% "kafka-journal-persistence"        % version
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -16,3 +16,5 @@ addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.2")
 addSbtPlugin("com.evolution" % "sbt-scalac-opts-plugin" % "0.0.9")
 
 addSbtPlugin("com.evolution" % "sbt-artifactory-plugin" % "0.0.2")
+
+addDependencyTreePlugin
