530f60719cb4d8cdfcbea99fb04839b38af0ceba
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-19 10:54:43 +0200

    Update cats-mtl to 1.3.1 (#479)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     private val version       = "2.7.0"
     private val effectVersion = "3.4.11"
     val core                  = "org.typelevel" %% "cats-core"           % version
-    val mtl                   = "org.typelevel" %% "cats-mtl"            % "1.2.1"
+    val mtl                   = "org.typelevel" %% "cats-mtl"            % "1.3.1"
     val effect                = "org.typelevel" %% "cats-effect"         % effectVersion
     val effectTestkit         = "org.typelevel" %% "cats-effect-testkit" % effectVersion
   }
