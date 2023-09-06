f4e64a0c8d3fb4dd0678905d862da833a74667de
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-06 14:02:32 +0200

    Update skafka to 16.0.3 (#533)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val catsHelperLogback = "com.evolutiongaming" %% "cats-helper-logback" % "3.7.0"
   val smetrics          = "com.evolutiongaming" %% "smetrics"            % "2.0.0"
   val scache            = "com.evolution"       %% "scache"              % "5.1.2"
-  val skafka            = "com.evolutiongaming" %% "skafka"              % "16.0.0"
+  val skafka            = "com.evolutiongaming" %% "skafka"              % "16.0.3"
   val sstream           = "com.evolutiongaming" %% "sstream"             % "1.0.1"
 
   object Cats {
