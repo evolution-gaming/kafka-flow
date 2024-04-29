967cbdea85498aa24edead2b579a4b3ddeb8358e
Z1kkurat <kshelopugin@gmail.com>
2024-04-29 13:45:48 +0200

    Add scassandra-based version of KeySchema; add integration test for KeySchema

--- persistence-cassandra/src/main/scala/com/evolutiongaming/kafka/flow/key/KeySchema.scala
+++ persistence-cassandra/src/main/scala/com/evolutiongaming/kafka/flow/key/KeySchema.scala
@@ -4,6 +4,7 @@ import cats.Monad
 import cats.syntax.all._
 import com.evolutiongaming.cassandra.sync.CassandraSync
 import com.evolutiongaming.kafka.journal.eventual.cassandra.CassandraSession
+import com.evolutiongaming.scassandra
 
 private[key] trait KeySchema[F[_]] {
 
@@ -44,6 +45,39 @@ private[key] object KeySchema {
     }
     def truncate = synchronize("KeySchema") {
       session.execute("TRUNCATE keys").first.void
+    }
+  }
+
+  def of[F[_]: Monad](
+    session: scassandra.CassandraSession[F],
+    synchronize: CassandraSync[F]
+  ): KeySchema[F] = new KeySchema[F] {
+    def create = synchronize("KeySchema") {
+      session
+        .execute(
+          """CREATE TABLE IF NOT EXISTS keys(
+          |application_id TEXT,
+          |group_id TEXT,
+          |segment BIGINT,
+          |topic TEXT,
+          |partition INT,
+          |key TEXT,
+          |created TIMESTAMP,
+          |created_date DATE,
+          |metadata TEXT,
+          |PRIMARY KEY((application_id, group_id, segment), topic, partition, key)
+          |)
+          |""".stripMargin
+        ) >>
+        session
+          .execute(
+            "CREATE INDEX IF NOT EXISTS keys_created_date_idx ON keys(created_date)"
+          )
+          .void
+    }
+
+    def truncate = synchronize("KeySchema") {
+      session.execute("TRUNCATE keys").void
     }
   }
 
