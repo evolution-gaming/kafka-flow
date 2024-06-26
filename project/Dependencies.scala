c2df59d323809181cdbefb0e86936851e59d761a
Scala Steward <scala_steward@virtuslab.com>
2024-06-26 14:22:51 +0000

    Update kafka-journal, ... to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   }
 
   object KafkaJournal {
-    private val version = "3.3.9"
+    private val version = "3.4.0"
     val journal         = "com.evolutiongaming" %% "kafka-journal"                    % version
     val cassandra       = "com.evolutiongaming" %% "kafka-journal-eventual-cassandra" % version
     val persistence     = "com.evolutiongaming" %% "kafka-journal-persistence"        % version
