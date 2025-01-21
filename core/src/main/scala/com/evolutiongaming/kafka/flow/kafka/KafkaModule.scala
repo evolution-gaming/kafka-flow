ff676e745459c1c4eb623e9311ab3a3ae35408b2
NikolajsCmihuns <nikolajs.cmihuns@gmail.com>
2025-01-21 14:19:31 +0200

    Pass SSL related settings to kafka healthcheck consumer (#669)
    * Pass SSL related settings to kafka healthcheck consumer
    
    * Add scala docs for KafkaModule of method

--- core/src/main/scala/com/evolutiongaming/kafka/flow/kafka/KafkaModule.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/flow/kafka/KafkaModule.scala
@@ -26,6 +26,10 @@ trait KafkaModule[F[_]] {
 
 object KafkaModule {
 
+  /** Creates kafka consumer and producer builders, and additionally launches kafka healthcheck mechanism which
+    * repeatedly sends and consumes messages to/from topic named 'healthcheck' (refer to
+    * [[KafkaHealthCheck.Config.default]])
+    */
   def of[F[_]: Async: FromTry: ToTry: ToFuture: LogOf](
     applicationId: String,
     config: ConsumerConfig,
@@ -47,7 +51,7 @@ object KafkaModule {
 
         val healthCheck = KafkaHealthCheck.of(
           KafkaHealthCheck.Config.default,
-          ConsumerConfig(common = commonConfig),
+          ConsumerConfig(common = commonConfig, saslSupport = config.saslSupport, sslSupport = config.sslSupport),
           ProducerConfig(common = commonConfig, saslSupport = config.saslSupport, sslSupport = config.sslSupport)
         )
         LogResource[F](KafkaModule.getClass, "KafkaHealthCheck") *> healthCheck
