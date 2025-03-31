fa0ea14e76bb5aecbbb240a7113d508b5e4799f5
Scala Steward <scala_steward@virtuslab.com>
2025-03-31 13:17:05 +0000

    Update cats-effect, cats-effect-testkit to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   object Cats {
     private val version       = "2.13.0"
-    private val effectVersion = "3.5.7"
+    private val effectVersion = "3.6.0"
     val core                  = "org.typelevel" %% "cats-core"           % version
     val mtl                   = "org.typelevel" %% "cats-mtl"            % "1.5.0"
     val effect                = "org.typelevel" %% "cats-effect"         % effectVersion
