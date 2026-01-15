7a90a2ab8792586f11aa9e60d5ef96b27795fd0e
Scala Steward <scala_steward@virtuslab.com>
2026-01-15 16:56:47 +0000

    Update kafka-journal to 6.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   }
 
   object KafkaJournal {
-    private val version = "6.1.2"
+    private val version = "6.2.0"
     val journal         = "com.evolution" %% "kafka-journal" % version
   }
 
