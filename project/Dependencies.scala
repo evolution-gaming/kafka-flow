588c0bb64a4147594197f5171d060d309d75aa46
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-05-07 12:20:42 +0200

    Update kafka-journal to 6.3.1 (#812)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   }
 
   object KafkaJournal {
-    private val version = "6.3.0"
+    private val version = "6.3.1"
     val journal         = "com.evolution" %% "kafka-journal" % version
   }
 
