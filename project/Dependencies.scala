f6a53cf72b601c4e6dd32b70d26769fddfedc0f6
Scala Steward <scala_steward@virtuslab.com>
2026-07-22 16:27:23 +0000

    Update sstream to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val smetrics          = "com.evolutiongaming" %% "smetrics"            % "2.4.5"
   val scache            = "com.evolution"       %% "scache"              % "6.0.1"
   val skafka            = "com.evolutiongaming" %% "skafka"              % "20.2.1"
-  val sstream           = "com.evolutiongaming" %% "sstream"             % "1.1.0"
+  val sstream           = "com.evolutiongaming" %% "sstream"             % "1.2.0"
   val scassandra        = "com.evolutiongaming" %% "scassandra"          % "5.5.0"
   val cassandraSync     = "com.evolutiongaming" %% "cassandra-sync"      % "4.0.0"
   val random            = "com.evolution"       %% "random"              % "1.0.5"
